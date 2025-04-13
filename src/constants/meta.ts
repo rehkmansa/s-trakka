ea6b124f7842808f3863a5190caac68d36ae7ad4
rehkmansa <godspowernathaniel25@gmail.com>
2025-04-13 21:43:47 +0100

    Add site constants.

---
+++ src/constants/meta.ts
@@ -0,0 +1,9 @@
+export const SITE_NAME = 'S-Traka';
+
+export const PLACEHOLDER_LINK = '/';
+
+export const NAV_LINKS = [
+  { label: 'Trade', href: PLACEHOLDER_LINK },
+  { label: 'Market Overview', href: PLACEHOLDER_LINK },
+  { label: 'Portfolio', href: PLACEHOLDER_LINK, active: true },
+];
